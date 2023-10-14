c571692e849613a1b6f60c701b7eef6b89af11b4
Nymul Islam Moon <towkir197islam@gmail.com>
2023-10-14 11:37:37 +0600

    Spanie installed and admin front page access off

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,10 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('index');
+// });
 
 Auth::routes();
 
-Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home');
